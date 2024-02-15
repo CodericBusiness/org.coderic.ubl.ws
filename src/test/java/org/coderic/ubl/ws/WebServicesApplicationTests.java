0682edc2af785e038df20d9cc25a4b877f9570b1
Neftali Yagua <neftaliyagua@fedoraproject.org>
2024-02-15 06:59:52 -0500

    first commit

---
+++ src/test/java/org/coderic/ubl/ws/WebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coderic.ubl.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
